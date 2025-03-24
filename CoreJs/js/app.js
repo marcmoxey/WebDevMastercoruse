466a2064f1d00a86615206a3229eb42efb12cf64
marcmoxey <moxeymarc@gmail.com>
2025-03-24 11:11:25 -0400

    add js file learned variables

---
+++ CoreJs/js/app.js
@@ -0,0 +1,30 @@
+
+// Variables
+
+// let - design to change 
+// const - design not to change
+const  testString = 'this is a test';
+const testNumber = 45.87;
+const testBoolean = true;
+
+//const testConst; // needs init value
+
+
+//testString = 'this is a BETTER TEST';
+console.log(testString + testString);
+
+// HOISTING 
+// var i; 
+// i = 10;
+// i = 5;
+// console.log(i);
+
+// var i = 2
+// console.log(i);
+
+// j = 10; 
+// j = 5;
+
+// console.log(j);
+
+// let j = 2; 
---
+++ CoreJs/js/exercises.js
@@ -0,0 +1,44 @@
+
+// Declare and log variables 
+const name = "Marc";
+let age = 23;
+let city = "NYC";
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+
+// Change variables values
+let favColor = "purple";
+console.log(favColor);
+favColor = "black";
+console.log(favColor);
+
+// Use 'const' and 'let;
+const country = "USA"; 
+//country = "UK"; 
+
+let hobby = "gaming";
+hobby = "tuning Cars";
+
+
+// Combine variables 
+let firstName = "Marc";
+let lastName = "Moxey";
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+// Swap values
+let a = 5;
+let b = 10;
+
+let temp = a; 
+console.log(temp);
+
+a = b; 
+console.log(a);
+
+b = temp;
+console.log(5);
+
