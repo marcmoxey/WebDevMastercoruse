cdbc09293c3922039b85f6d45bac13cf8637ebc5
marcmoxey <moxeymarc@gmail.com>
2025-03-31 08:25:25 -0500

    edit learn best practice

--- CoreJs/js/app.js
+++ CoreJs/js/app.js
@@ -413,3 +413,22 @@ console.log(b);
 
 
 // Best Practices 
+
+/* 
+
+1. Add 'use strict'; to the top of every file and IIFE 
+
+2. Do not use var - use let ir const instead. Prefer const. 
+
+3. Naming - use camelCase for variables, functions etc. and PascalCase for classes 
+
+4. Use IIFE's whenever it makes sens 
+
+5. Just because you can,  doesn't mean you should
+
+6. Use a separate file for your JavaScript
+
+7. Use semi-colons.
+
+8. Don't Assume
+*/
