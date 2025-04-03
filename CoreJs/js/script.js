871048593a60e883beca1d27ca73a754caf0f06d
marcmoxey <moxeymarc@gmail.com>
2025-04-03 14:05:08 -0400

    edit

---
+++ CoreJs/js/script.js
@@ -0,0 +1,28 @@
+(function(app){
+    'use strict';
+    app.divide = function(x,y) {
+        //console.log(x/y);
+        
+        return x / y
+    };
+
+    app.complicatedFormula = function(x) {
+        let result = x * 3 + 2;
+        //console.log(result);
+        result += 4;
+        //console.log(result);
+        result = this.divide(result,2)
+        //console.log(result);
+        return result;
+    }
+})(window.app = window.app || {});
+
+console.log(app.complicatedFormula(23));
+// console.error('This is an error')
+// console.warn("This is a warning");
+
+
+// 23 * 3 = 69 
+// 69 + 2 = 71 
+// 71 + 4 = 75
+// 75 / 2 = 37.5
