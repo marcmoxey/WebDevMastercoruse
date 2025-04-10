0fcdb5da0fdc6b2ec2e212b4a368f2d0f65966ac
marcmoxey <moxeymarc@gmail.com>
2025-04-10 15:36:11 -0400

    edit manipulating elements

--- JSInTheDOM/js/exercise.js
+++ JSInTheDOM/js/exercise.js
@@ -18,3 +18,32 @@ let text = document.getElementById('text');
 removeBtn.addEventListener('click',function(){
     text.remove();
 });
+
+
+// Manipulating Elements
+let newStyle = document.getElementById('newStyle');
+newStyle.style.backgroundColor = 'yellow';
+newStyle.style.fontSize = '4.5rem';
+
+let inputText = document.getElementById('username');
+inputText.value = 'Update Name';
+
+
+let boldMsg = document.getElementById('someText');
+boldMsg.innerHTML = '<strong>This is a strong tag</strong>'
+
+let disappearText = document.getElementById('magic');
+let magicBtn = document.getElementById('magicBtn'); 
+
+magicBtn.addEventListener('click', function() {
+    if(disappearText.style.display === 'none'){
+        disappearText.style.display = 'block';
+    } else {
+        disappearText.style.display = 'none';
+    }
+});
+
+const task = document.getElementById('task');
+const newTask = document.createElement('li');
+newTask.textContent = 'Task 2';
+task.appendChild(newTask);
