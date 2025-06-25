f363fa3878b2c6c5e8fc84815236de8b9a00cc38
Marc Moxey <moxeymarc@gmail.com>
2025-06-25 10:20:50 -0400

    Add mnore  test and fix app.js file

--- WebTools/app.js
+++ WebTools/app.js
@@ -3,9 +3,16 @@
 
 // test = 'hello world';
 
-(function(app) {
-    app.add = (x,y) => x + y; 
-    app.subtract = (x,y) => y -x;
-}) (window.app = window.app || {});
+// (function(app) {
+//     app.add = (x,y) => x + y; 
+//     app.subtract = (x,y) => x - y;
+// })(window.app = window.app || {});
+
+// module.exports = app;
+
+const app = {};
+
+app.add = (x, y) => x + y;
+app.subtract = (x, y) => x - y;
 
 module.exports = app;
--- WebTools/app.test.js
+++ WebTools/app.test.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
-test('5 ++2 should be 7', () => {
-    expect(app.add(5,2)).toBe(7);
+test('5 + 2 should be 7', () => {
+  expect(app.add(5, 2)).toBe(7);
+});
+
+test('8 - 5 should be 3', () => {
+  expect(app.subtract(8, 5)).toBe(3); // <-- FIXED
 });
