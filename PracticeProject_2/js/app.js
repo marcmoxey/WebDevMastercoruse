4376bbca740af63389586b4e583ff73dc9e42279
Marc <moxiem@nychhc.org>
2025-08-19 15:50:53 -0400

    Add dynamic to portolio

--- PracticeProject_2/js/app.js
+++ PracticeProject_2/js/app.js
@@ -1,106 +1,142 @@
-(function(app) {
+(function (app) {
+  app.portfolioItems = [];
+  app.selectedItem = {};
 
-    app.portfolioItems = [];
-    app.selectedItem = {};
+  app.homepage = function () {
+    setCopyrightDate();
+    wireContactForm();
+  };
 
-    app.homepage = function() {
-        setCopyrightDate();
-        wireContactForm();
-    }; 
+  app.portfolio = function () {
+    setCopyrightDate();
+    loadPageData();
+    loadPortfolioPageData();
+  };
 
-    app.portfolio = function() {
-        setCopyrightDate();
+  app.workItem = function () {
+    setCopyrightDate();
+    loadPageData();
+    loadSpecificItem();
+    updateItemPage();
+  };
+
+  function setCopyrightDate() {
+    const date = new Date();
+    document.getElementById('copyrightYear').innerText = date.getFullYear();
+  }
+
+  function wireContactForm() {
+    const contactForm = document.getElementById('contact-form');
+    contactForm.onsubmit = contactFormSubmit;
+  }
+
+  function contactFormSubmit(e) {
+    e.preventDefault();
+    const contactForm = document.getElementById('contact-form');
+    const name = contactForm.querySelector('#name');
+    const email = contactForm.querySelector('#email');
+    const message = contactForm.querySelector('#message');
+
+    const mailTo = `mailto:${email.value}?subject=Contact From ${name.value}&body=${message.value}`;
+    window.open(mailTo);
+
+    name.value = '';
+    email.value = '';
+    message.value = '';
+  }
+
+  async function loadPageData() {
+    const cacheData = sessionStorage.getItem('site-data');
+
+    if (cacheData !== null) {
+      app.portfolioItems = JSON.parse(cacheData);
+    } else {
+      const rawData = await fetch('sitedata.json');
+      const data = await rawData.json();
+      app.portfolioItems = data;
+      sessionStorage.setItem('site-data', JSON.stringify(data));
+    }
+  }
+
+  // https://iamticorey.com?course=1&test=No%20Way
+  // https://allyreynolds.com?item=2
+  function loadSpecificItem() {
+    const params = new URLSearchParams(window.location.search);
+    let item = Number.parseInt(params.get('item'));
+
+    if (item > app.portfolioItems.length || item < 1) {
+      // our items: 1-3
+      // our array ids should be 0-2
+      // our array length should be 3
+      item = 1;
     }
 
-    app.workItem = function() {
-        setCopyrightDate();
-        loadPageData();
-        loadSpecificItem();
-        updateItemPage();
-    };
+    app.selectedItem = app.portfolioItems[item - 1];
+    app.selectedItem.id = item;
+  }
 
-    function setCopyrightDate() {
-        const date = new Date();
-        document.getElementById('copyrightYear').innerText = date.getFullYear();
+  function updateItemPage() {
+    const header = document.getElementById('work-item-header');
+    header.innerText = `0${app.selectedItem.id}. ${app.selectedItem.title}`;
+
+    const image = document.getElementById('work-item-image');
+    image.src = app.selectedItem.largeImage;
+    image.alt = app.selectedItem.largeImageAlt;
+
+    const projectText = document.querySelector('#project-text');
+    projectText.innerText = app.selectedItem.projectText;
+
+    const originalTechList = document.querySelector('#technologies-text ul');
+    const technologySection = document.getElementById('technologies-text');
+    const ul = document.createElement('ul');
+
+    app.selectedItem.technologies.forEach((el) => {
+      const li = document.createElement('li');
+      li.innerText = el;
+      ul.appendChild(li);
+    });
+
+    originalTechList.remove();
+    technologySection.appendChild(ul);
+
+    const challengesText = document.querySelector('#challenges-text');
+    challengesText.innerText = app.selectedItem.challengesText;
+  }
+
+  function loadPortfolioPageData() {
+    const originalItems = document.querySelectorAll('.highlight');
+    const main = document.getElementById('portfolio-main');
+    const newItems = [];
+
+    for (let i = 0; i < app.portfolioItems.length; i++) {
+      const el = app.portfolioItems[i];
+      const highlight = document.createElement('div');
+      highlight.classList.add('highlight');
+      if (i % 2 > 0) {
+        highlight.classList.add('invert');
+      }
+
+      const textDiv = document.createElement('div');
+      const h2 = document.createElement('h2');
+      const a = document.createElement('a');
+
+      const titleWord = el.title.split(' ');
+      h2.innerHTML = el.title;
+      a.href = `workitem.html?item=${i + 1}`;
+      a.innerText = 'see more';
+      textDiv.appendChild(h2);
+      textDiv.appendChild(a);
+      highlight.appendChild(textDiv);
+
+      const img = document.createElement('img');
+      img.src = el.smallImage;
+      img.alt = el.smallImageAlt;
+      highlight.appendChild(img);
+
+      newItems.push(highlight);
     }
 
-    function wireContactForm() {
-        const contactForm= document.getElementById('contact-form');
-        contactForm.onsubmit = contactFormSubmit;
-    }
-
-    function contactFormSubmit(e) {
-        e.preventDefault();
-        const contactForm = document.getElementById('contact-form');
-        const name = contactForm.querySelector('#name')
-        const email = contactForm.querySelector('#email');
-        const message = contactForm.querySelector('#message');
-
-        const mailTo = `mailto:${email.value}?subject=Contact From ${name.value}&body=${message.value}`;
-        window.open(mailTo);
-
-        name.value = '';
-        email.value = '';
-        message.value = '';
-    }
-
-    async function loadPageData() {
-        const cacheData = sessionStorage.getItem('site-data');
-
-        if(cacheData !== null) {
-            app.portfolioItems = JSON.parse(cacheData);
-        } else {
-            const rawData = await fetch('sitedata.json');
-            const data = await rawData.json();
-            app.portfolioItems = data;
-            sessionStorage.setItem('site-data',JSON.stringify(data));
-      
-        }
-    }
-
-    // https://iamticorey.com?course=1&test=No%20Way
-    // https://allyreynolds.com?item=2
-    function loadSpecificItem(){
-        const params = new URLSearchParams(window.location.search);
-        let item = Number.parseInt(params.get('item'))
-
-        if (item > app.portfolioItems.length || item < 1) {
-            // our items: 1-3
-            // our array ids should be 0-2
-            // our array length should be 3
-            item = 1
-        }
-
-        app.selectedItem = app.portfolioItems[item - 1];
-        app.selectedItem.id = item;
-    }
-
-    function updateItemPage() {
-        const header = document.getElementById('work-item-header');
-        header.innerText = `0${app.selectedItem.id}. ${app.selectedItem.title}`;
-
-        const image = document.getElementById('work-item-image');
-        image.src = app.selectedItem.largeImage;
-        image.alt = app.selectedItem.largeImageAlt;
-
-        const projectText = document.querySelector('#project-text');
-        projectText.innerText = app.selectedItem.projectText;
-
-
-        const originalTechList = document.querySelector('#technologies-text ul');
-        const technologySection = document.getElementById('technologies-text');
-        const ul = document.createElement('ul');
-
-        app.selectedItem.technologies.forEach(el => {   
-            const li = document.createElement('li');
-            li.innerText = el;
-            ul.appendChild(li);
-        });
-        
-        originalTechList.remove();
-        technologySection.appendChild(ul);
-        
-        const challengesText = document.querySelector('#challenges-text');
-        challengesText.innerText = app.selectedItem.challengesText;
-    }
-})(window.app = window.app || {});
+    originalItems.forEach((el) => el.remove());
+    newItems.forEach((el) => main.appendChild(el));
+  }
+})((window.app = window.app || {}));
